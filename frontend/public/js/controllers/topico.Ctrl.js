74be96447d0d3a5325533e5655c87c6981101b06
bielfarias2002@gmail.com <yuridosnajos@gmail.com>
2019-12-19 15:39:10 -0300

    commit-correto

--- frontend/public/js/controllers/topico.Ctrl.js
+++ frontend/public/js/controllers/topico.Ctrl.js
@@ -3,11 +3,18 @@ angular.module('projeto').controller('topicoCtrl', function($scope, topicosAPI){
     $scope.idTopico=localStorage.getItem('idtopico')
 
 	const getTopico=(id)=>{
-        
+        topicosAPI.topicoPorId(id).then((result)=>{
+            $scope.topico=result.data
+        }).catch((err)=>{
+            $scope.error='não foi possivel carregar os dados'
+            $scope.error2=err
+        })
+
     }
+    
 
 
     if($scope.idTopico){
-        
+        getTopico()
     }
 })
