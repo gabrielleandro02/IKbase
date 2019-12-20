9453aea9dc06ed60f67ae590b8f9cf94b5fe68d6
gabrielleandro02 <bielfarias2002@gmail.com>
2019-12-20 20:27:49 -0300

    commit-gabriel5

---
+++ frontend/public/js/ajuda.js
@@ -0,0 +1,19 @@
+function mostra () {
+    var tutorial1 = document.getElementById('tutorial-1')
+
+    if(tutorial1.style.display === 'none'){
+        tutorial1.style.display = 'block'
+    }else{
+        tutorial1.style.display = 'none'
+    }
+}
+
+function mostra2 () {
+    var tutorial2 = document.getElementById('tutorial-2')
+
+    if(tutorial2.style.display === 'none'){
+        tutorial2.style.display = 'block'
+    }else{
+        tutorial2.style.display = 'none'
+    }
+}
